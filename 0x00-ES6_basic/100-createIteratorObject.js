1d07b31bee6f32ba2f36210896383a91d62c8b13
kennyChukwuebuka <kenkinado@gmail>
2024-04-08 23:09:41 +0100

    fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,16 @@
 export default function createIteratorObject(report) {
   function* employeeIterator() {
     for (const department in report.allEmployees) {
-      const employees = report.allEmployees[department];
-      for (const employee of employees) {
-        yield employee;
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
+        }
       }
     }
   }
 
   return {
-    [Symbol.iterator]: employeeIterator
+    [Symbol.iterator]: employeeIterator,
   };
 }
