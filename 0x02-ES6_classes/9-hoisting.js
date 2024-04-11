050f2a2cf8b86fdf514e3cd311e859f5e7aad5a6
kennyChukwuebuka <kenkinado@gmail>
2024-04-11 20:07:08 +0100

    fix my code

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,5 @@
-export class HolbertonClass {
+/*eslint-disable*/
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +14,7 @@ export class HolbertonClass {
   }
 }
 
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
